b77d06af6d4bdcb0edb17eb0346c3de0e29fe866
Zak Djermani <zak.djermani@gmail.com>
2024-06-06 12:18:03 +0200

    set lists controller & bookmarks controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+
+puts 'Cleaning database...'
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/popular'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts 'Creating movies...'
+
+movies['results'].each do |result|
+  movie = {
+    title: result['title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{result['poster_path']}",
+    rating: result['vote_average']
+  }
+  Movie.create!(movie)
+  puts "Created #{movie['title']}"
+end
+puts 'Finished!'
+
+List.create!({ name: 'Action' })
+List.create!({ name: 'Comedy' })
